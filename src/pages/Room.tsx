85e1c46d1ab3a0b085707a1e354c587292e1e396
Christian Mendel <chr9mendel@gmail.com>
2022-02-02 15:57:51 -0300

    commit 3

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,5 +1,5 @@
-import { FormEvent, useState } from 'react'
-import {useParams} from 'react-router-dom'
+import { FormEvent, useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
 
 import logoImg from '../assets/images/logo.svg'
 import { Button } from '../Components/Button'
@@ -10,65 +10,103 @@ import { database } from '../Services/firebase'
 import '../styles/room.css'
 
 type RoomParams = {
-    id:string;
+    id: string;
 }
 
-export function Room(){
-    const {user} = useAuth()
-    const params = useParams<RoomParams>()
-    const roomId = params.id === undefined? '' : params.id;
+type FireBaseQuestions = Record<string, {
+    author: {
+        nome: string;
+        avatar: string;
+    }
+    content: string;
+    isHighligted: boolean;
+    isAnswered: boolean;
+}>
 
-    const [newQuestion,setNewQuestion]= useState('')
-    
-    async function handleSendQuestion(e:FormEvent){
+export function Room() {
+    const { user } = useAuth()
+    const params = useParams<RoomParams>()
+    const roomId = params.id === undefined ? '' : params.id;
+
+    const [newQuestion, setNewQuestion] = useState('')
+
+    useEffect(() => {
+        const roomRef = database.ref(`rooms/${roomId}`)
+        roomRef.once('value', room => {
+            const databaseRoom = room.val()
+            const firebaseQuestions: FireBaseQuestions = databaseRoom.questions ?? {}
+            const parcedQuestions = Object.entries(firebaseQuestions).map(([key, value]) => {
+                return {
+                    id: key,
+                    content: value.content,
+                    author: value.author,
+                    isHighligted: value.isHighligted,
+                    isAnswered: value.isAnswered,
+                }
+            })
+
+            console.log(parcedQuestions)
+        })
+    }, [roomId])
+
+    async function handleSendQuestion(e: FormEvent) {
         e.preventDefault()
 
-        if(newQuestion.trim()===''){
+        if (newQuestion.trim() === '') {
             return;
         }
-        if(!user){
+        if (!user) {
             throw new Error('Erro, faça login na sua conta do Google!')
         }
-        
+
         const question = {
-            content:newQuestion,
-            author:{
-                name:user.nome,
-                avatar:user.avatar,
+            content: newQuestion,
+            author: {
+                name: user.nome,
+                avatar: user.avatar,
 
             },
             isHighligted: false,
-            isAnswered:false
+            isAnswered: false
         }
 
         await database.ref(`rooms/${roomId}/questions`).push(question)
         setNewQuestion('')
     }
-    return(
-      <div id="page_room">
-          <header>
-              <div className="content_header">
-                  <img src={logoImg} alt="logoImg" className='logoImg'/>
-                  <RoomCode code={roomId}/>
-              </div>
-          </header>
-
-          <main>
-            <div className='room_title'>
-                <h1>Sala React</h1>
-                <span className='span_pergs'>4 perguntas</span>
-            </div>
-            <form onSubmit={handleSendQuestion}>
-                <textarea placeholder='O que você quer perguntar?'
-                    onChange={e => setNewQuestion(e.target.value)}
-                    value={newQuestion}
-                />
-                <div className='form_footer'>
-                    <span className='span_footer'>Para enviar uma pergunta, <button className='span_button'>faça seu login</button></span>
-                    <Button type='submit' disabled={!user}>Enviar pergunta</Button>
+    return (
+        <div id="page_room">
+            <header>
+                <div className="content_header">
+                    <img src={logoImg} alt="logoImg" className='logoImg' />
+                    <RoomCode code={roomId} />
                 </div>
-            </form>
-          </main>
-      </div>
+            </header>
+
+            <main>
+                <div className='room_title'>
+                    <h1>Sala React</h1>
+                    <span className='span_pergs'>4 perguntas</span>
+                </div>
+                <form onSubmit={handleSendQuestion}>
+                    <textarea placeholder='O que você quer perguntar?'
+                        onChange={e => setNewQuestion(e.target.value)}
+                        value={newQuestion}
+                    />
+                    <div className='form_footer'>
+                        {user ? (
+                            <div className='user-info'>
+                                <img src={user.avatar} alt={user.nome} className='user-img' />
+                                <span className='user-nome'>{user.nome}</span>
+                            </div>
+                        ) : (
+                            <div>
+                                <span className='span_footer'>Para enviar uma pergunta, <button className='span_button'>faça seu login</button></span>
+                            </div>
+                        )}
+                        <Button type='submit' disabled={!user}>Enviar pergunta</Button>
+                    </div>
+                </form>
+            </main>
+        </div>
     )
 }
